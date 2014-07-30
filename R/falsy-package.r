6e5f562243da4105dfc55fc6a7dc0d8f168a44c5
Gabor Csardi <csardi.gabor@gmail.com>
2014-07-30 13:55:02 -0400

    Initial version.
    Fairly complete, tests missing.

---
+++ R/falsy-package.r
@@ -0,0 +1,180 @@
+#' falsy.
+#'
+#' We define a set of objects as \sQuote{falsy}. Every other values are
+#' \sQuote{truthy}. This is inspired by other scripting languages like
+#' Python and Ruby. See the \code{\%||\%} and the \code{\%&&\%} operators
+#' on how to write concise conditional code using truthy and falsy values.
+#'
+#' @name falsy
+#' @docType package
+NULL
+
+#' Is an object FALSY
+#'
+#' Falsy objects are:
+#' \itemize{
+#'  \item \code{NULL}
+#'  \item \code{FALSE}
+#'  \item \code{0L}, integer zero value.
+#'  \item \code{0}, real zero value.
+#'  \item \code{0+0i}, complex zero value.
+#'  \item \code{""}, character scalar that is the empty string.
+#'  \item \code{00}, one byte raw vector with zero value.
+#'  \item Empty vectors. I.e. \code{logical()}, \code{integer()},
+#'        \code{double()}, \code{complex()}, \code{character()}
+#'        and \code{raw()}.
+#'  \item Empty list.
+#'  \item Object from the \code{try-error} class.
+#' }
+#' Everything else is truthy.
+#'
+#' @param object The object.
+#' @return Logical scalar.
+#'
+#' @export
+#' @examples
+#' ## NULL is falsy
+#' is_falsy(NULL)
+#'
+#' ## So is the empty string
+#' is_falsy("")
+#'
+#' ## But the zero string is not
+#' is_falsy("0")
+#'
+#' ## Clasess are truthy
+#' is_falsy(factor())
+#'
+#' ## Errors reported by try() are falsy
+#' is_falsy(try(a + "string", silent = TRUE))
+
+is_falsy <- function(object) {
+  is.null(object) ||
+    identical(object, FALSE) ||
+    identical(object, 0L) ||
+    identical(object, 0.0) ||
+    identical(object, 0+0i) ||
+    identical(object, "") ||
+    identical(object, as.raw(0)) ||
+    identical(object, logical()) ||
+    identical(object, integer()) ||
+    identical(object, double()) ||
+    identical(object, complex()) ||
+    identical(object, character()) ||
+    identical(object, raw()) ||
+    identical(object, list()) ||
+    inherits(object, "try-error")
+}
+
+#' Is an object TRUY
+#'
+#' @param object The object.
+#'
+#' @export
+#' @examples
+#' ## Non-zero numbers are truthy
+#' is_truthy(1)
+#'
+#' ## So are non-empty vectors that are not the zero scalar
+#' is_truthy(c(1))
+#'
+#' ## Matrices are truthy, even if they are empty,
+#' ## because they are a class
+#' is_truthy(matrix(nrow=0, ncol=0))
+#'
+#' ## But empty vectors are falsy
+#' is_truthy(numeric())
+
+is_truthy <- function(object) {
+  ! is_falsy(object)
+}
+
+#' Robust 'or' operator working with truthy and falsy values
+#'
+#' @rdname or
+#' @name or
+#' @param lhs Left hand side, always evaluated.
+#' @param rhs Right hand side, only evaluated if \code{lhs}
+#' evaluates to FALSY.
+#'
+#' @export
+#' @examples
+#'
+#' \donttest{
+#' ## Set colors from colorspace package with a fallback
+#' col <- try(colorspace::rainbow_hcl(5), silent = TRUE) %||% rainbow(5)
+#'
+#' ## Simpler with silent try
+#' col <- try_quietly(colorspace::rainbow_hcl(5)) %||% rainbow(5)
+#'
+#' ## Message if foobar is not installed
+#' try_quietly(library(foobar)) %||% message("no foobar")
+#'
+#' ## Message if list is empty
+#' l <- list(1,2,3)
+#' l %||% message("list is empty")
+#'
+#' l <- list()
+#' l %||% message("list is empty")
+#' }
+
+`%||%` <- function(lhs, rhs) {
+  lhs <- substitute(lhs)
+  rhs <- substitute(rhs)
+  lres <- withVisible(eval(lhs, envir = parent.frame()))
+  if (is_falsy(lres$value)) {
+    eval(rhs, envir = parent.frame())
+  } else {
+    if (lres$visible) { lres$value } else { invisible(lres$value) }
+  }
+}
+
+#' Robust 'and' operator working with truthy and falsy values
+#'
+#' @rdname and
+#' @name and
+#' @param lhs Left hand side, always evaluated.
+#' @param rhs Right hand side, evaluated only if \code{lhs}
+#' evaluates to TRUTHY.
+#'
+#'@export
+#' @examples
+#'
+#' ## Shift to zero
+#' v <- 5:10
+#' v %&&% { v <- v - min(v) }
+#' v
+#'
+#' ## It works if the vector is empty
+#' v <- numeric()
+#' v %&&% { v <- v - min(v) }
+#' v
+
+`%&&%` <- function(lhs, rhs) {
+  lhs <- substitute(lhs)
+  rhs <- substitute(rhs)
+  lres <- withVisible(eval(lhs, envir = parent.frame()))
+  if (is_truthy(lres$value)) {
+    eval(rhs, envir = parent.frame())
+  } else {
+    if (lres$visible) { lres$value } else { invisible(lres$value) }
+  }
+}
+
+#' Try quietly
+#'
+#' We just call \code{try} and set its \code{silent} argument to
+#' \code{TRUE}.
+#'
+#' @param expr The R expression to evaluate.
+#'
+#' @export
+#' @examples
+#'
+#' \donttest{
+#' try_quietly(library(foobar)) %||% message("Skipping foobar")
+#' }
+
+try_quietly <- function(expr) {
+  try(expr, silent = TRUE)
+}
